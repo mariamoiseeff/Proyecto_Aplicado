a520594ba4b79ecf7166e5de6b5a06fd6ff73617
guadamargiotta <margiottaguadalupe@gmail.com>
2026-06-11 21:29:56 -0300

    Update validacion.py

--- src/validacion.py
+++ src/validacion.py
@@ -26,3 +26,32 @@ def validar_input(numero):
      raise ValueError("el numero ingresado no puede ser negativo, debe ser positivo")
   
   return int(numero)
+  
+
+def validar_eleccion_graficos(num):
+    '''
+    Valida que el numero ingresado por el usuario sea tipo de dato correcto 
+    y se encuentre dentro del rango indicado(1-7).
+    Parametros:
+    ----------
+    num : int
+        Numero ingresado del usuario para visualizar graficos.
+
+    Returns:
+    -------
+    num: int
+    
+    Raises:
+    -------
+    ValueError
+      Si el numero ingresado por el usuario es distinto a los 
+      graficos que estan disponibles.
+    TypeError
+      Si el numero ingresado es un tipo de dato incorrecto. 
+    '''
+    if type(num) != int:
+        raise TypeError("el dato ingresado es incorrecto, debe ser un numero entero")
+    if num < 1 or num > 7:
+        raise ValueError("el numero ingresado debe estar entre 1 y 7 ")
+        
+    return num
